43b220f5122164b86569c650f3659a19e3e8f519
Elizabeth <elizabeth.por@gmail.com>
2022-01-13 11:14:15 -0600

    Completed assignment

---
+++ example.md
@@ -0,0 +1,11 @@
+# Here is my notes about arrays
+
+Arrays can do a lot of things
+
+If I want to find something I can use `.find`. Here is an example.
+
+```javascript
+let variable = 7
+variable + 10
+let myArray = [1,2,3,4,5]
+```
--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = [
+    "Milo", 
+    "Otis", 
+    "Garfield"
+]
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat(name) { 
+    return [...cats, name];
+}
+function prependCat(name) {
+    return [name, ...cats];
+}
+function removeLastCat() {
+ //   const noLastCatArray = [cats.slice(-1)];
+ //   return noLastCatArray;
+    return [cats.slice(0, -1)];
+}
+function removeFirstCat() {
+ //   const noFirstCat = [cats.slice(1)];
+ //   return noFirstCat;
+    return [cats.slice(1)];
+}
